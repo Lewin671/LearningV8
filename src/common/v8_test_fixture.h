6077da09c10a2e52dd4a58c440ed6e31377a44d6
Lewin671 <2596736318@qq.com>
2022-10-16 01:52:43 +0800

    feat: add gtest lib and test code

---
+++ src/common/v8_test_fixture.h
@@ -0,0 +1,63 @@
+#ifndef TEST_V8_TEST_FIXTURE_H_
+#define TEST_V8_TEST_FIXTURE_H_
+
+#include <stdlib.h>
+#include <src/objects/objects.h>
+#include "gtest/gtest.h"
+#include "v8.h"
+#include "libplatform/libplatform.h"
+
+extern void _v8_internal_Print_Object(void* object);
+
+namespace i = v8::internal;
+
+class V8TestFixture : public ::testing::Test {
+public:
+    template<typename T>
+    static void print_local(v8::Local<T> obj) {
+        _v8_internal_Print_Object(*((v8::internal::Object**)*obj));
+    }
+    template<typename T>
+    static void print_handle(i::Handle<T> h) {
+        _v8_internal_Print_Object(*((v8::internal::Object**)h.location()));
+    }
+protected:
+    static std::unique_ptr<v8::Platform> platform_;
+    static std::unique_ptr<v8::ArrayBuffer::Allocator> allocator_;
+    static v8::Isolate::CreateParams create_params_;
+    v8::Isolate* isolate_;
+
+    static void SetUpTestCase() {
+        v8::V8::InitializeExternalStartupData("fixture");
+        platform_ = v8::platform::NewDefaultPlatform();
+        allocator_.reset(v8::ArrayBuffer::Allocator::NewDefaultAllocator());
+        create_params_.array_buffer_allocator = allocator_.get();
+        v8::V8::InitializePlatform(platform_.get());
+        v8::V8::Initialize();
+    }
+
+    static void TearDownTestCase() {
+        v8::V8::ShutdownPlatform();
+    }
+
+    virtual void SetUp() {
+        isolate_ = v8::Isolate::New(create_params_);
+    }
+
+    virtual void TearDown() {
+        isolate_->Dispose();
+    }
+
+    v8::internal::Isolate* asInternal(v8::Isolate* isolate) {
+        return reinterpret_cast<v8::internal::Isolate*>(isolate);
+    }
+
+
+private:
+};
+
+std::unique_ptr<v8::Platform> V8TestFixture::platform_;
+v8::Isolate::CreateParams V8TestFixture::create_params_;
+std::unique_ptr<v8::ArrayBuffer::Allocator> V8TestFixture::allocator_;
+
+#endif  // TEST_V8_TEST_FIXTURE_H_
---
+++ src/object_test.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "common/v8_test_fixture.h"
+#include "v8.h"
+
+using namespace v8;
+
+class MaybeTest : public V8TestFixture {
+};
+
+TEST_F(MaybeTest, Maybe) {
+    std::cout<<"test started"<<std::endl;
+    bool cond = true;
+    Maybe<int> maybe = cond ? Just<int>(10) : Nothing<int>();
+    EXPECT_TRUE(maybe.IsJust());
+    EXPECT_FALSE(maybe.IsNothing());
+    maybe.Check();
+
+    int nr = maybe.ToChecked();
+    EXPECT_EQ(nr, 10);
+    EXPECT_EQ(maybe.FromJust(), 10);
+
+    Maybe<int> nothing = Nothing<int>();
+    int value = nothing.FromMaybe(22);
+    EXPECT_EQ(value, 22);
+}
+
+/*
+ * I think the intention with a type Maybe<void> is that we don't really
+ * care/want to have a value in the Maybe apart from that is empty or
+ * something. So instead of having a bool and setting it to true just
+ * have void and return an empty. I think this signals the intent of a
+ * function better as one might otherwise wonder what the value in the maybe
+ * represents.
+ */
+Maybe<void> doit(int x) {
+    if (x == -1) {
+        return Nothing<void>();
+    }
+    return JustVoid();
+}
+
+TEST_F(MaybeTest, MaybeVoid) {
+    std::cout<<"test1 started"<<std::endl;
+
+    Maybe<void> maybe = JustVoid();
+    EXPECT_FALSE(maybe.IsNothing());
+
+    Maybe<void> maybe_nothing = Nothing<void>();
+    EXPECT_TRUE(maybe_nothing.IsNothing());
+
+    EXPECT_TRUE(doit(-1).IsNothing());
+    EXPECT_TRUE(doit(1).IsJust());
+}
